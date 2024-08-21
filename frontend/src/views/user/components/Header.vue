efe2db26bd1839ae940c5ac62207477d963e8295
mor-quydp <quydp@mor.com.vn>
2024-08-21 14:35:25 +0700

    config header

--- frontend/src/views/user/components/Header.vue
+++ frontend/src/views/user/components/Header.vue
@@ -1,43 +1,68 @@
 <template>
-  <header class="m-header w-100 d-flex justify-content-between align-items-center">
-    <div class="m-header_left d-flex align-items-center gap-5">
-      <div class="m-header-logo">
-        <img :src="require('@public/assets/icons/logo.svg')" alt="">
+  <header class="m-header w-100 d-flex flex-column justify-content-between align-items-center">
+    <div class="w-100 d-flex justify-content-between align-items-center ms-top_header">
+      <div>
+        Welcome to NobleGoods online eCommerce store.
       </div>
-      <div class="m-header-menu d-flex gap-3">
-        <div v-for="item in menus">
-          {{ item }}
+      <div class="social-contact_container d-flex justify-content-between align-items-center">
+        <div class="pe-3">Theo dõi:</div>
+        <div class="social-contact_list d-flex gap-2">
+          <div class="social-contact_item twitter"></div>
+          <div class="social-contact_item facebook"></div>
+          <div class="social-contact_item pinterest"></div>
+          <div class="social-contact_item youtube"></div>
+          <div class="social-contact_item instagram"></div>
         </div>
       </div>
     </div>
-    <div class="m-header_right d-flex align-items-center gap-3">
-      <div class="m-header-search">
-        <div class="m-search_form flex-row d-flex flex-grow-1 align-items-center">
-          <InputText type="search" class="ms-input_search" :placeholder="$t('search_prompt')"/>
-          <div class="icon24 icon search-right search"></div>
+    <div class="w-100 d-flex justify-content-between align-items-center ms-middle_header">
+      <div class="m-logo_container d-flex align-items-center">
+        <div class="logo"></div>
+        <div class="title">Noble</div>
+      </div>
+      <div class="m-search_form flex-row d-flex flex-grow-1 align-items-center">
+        <InputText type="search" v-model="value" class="ms-input_search" :placeholder="$t('search_prompt')"/>
+        <div class="icon24 icon search-right search"></div>
+      </div>
+      <div class="m-header_actions d-flex">
+        <div class="d-flex cart pointer flex-column justify-content-center ms-4 me-4 item-action">
+          <div class="icon"></div>
+          <div class="title">2</div>
+        </div>
+        <div class="d-flex love pointer flex-column align-items-center justify-content-center me-4 item-action">
+          <div class="icon"></div>
+        </div>
+        <div class="d-flex profile pointer flex-column align-items-center justify-content-center item-action">
+          <div class="icon"></div>
         </div>
       </div>
-      <div class="d-flex align-items-center justify-content-between flex-1">
-        <div class="group-profile d-flex align-items-center gap-3">
-          <div class="d-flex gap-3 align-items-center group-icons">
-            <div class="icon-heart_black icon-w24"></div>
-            <div class="icon-simple_cart-black icon-w24"></div>
-          </div>
-          <div class="group_user-info">
-            <Button type="button" icon="pi pi-ellipsis-v" @click="toggle" aria-haspopup="true"
-                    class="ms-btn bg-transparent"
-                    aria-controls="overlay_menu">
-              <div class="d-flex gap-2 align-items-center">
-                <div class="icon icon-w40">
-                  <img :src="require('@public/assets/images/avatar/162971076.jpeg')" class="m-user_avatar" alt="">
-                </div>
-                <div class="user-info">Phú Quý</div>
-              </div>
-            </Button>
-            <Menu ref="menu" id="overlay_menu" :model="userActions" :popup="true"/>
+    </div>
+    <div class="w-100 d-flex justify-content-between align-items-center ms-bottom_header">
+      <div class="left-side d-flex align-items-center gap-3">
+        <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Category"
+                  class="ms-category"/>
+        <div class="left-side_item align-items-center justify-content-center">
+          <div class="icon map"></div>
+          <div class="title">
+            Track Order
           </div>
         </div>
-        <div class="icon24 icon search search-mobile"></div>
+        <div class="left-side_item align-items-center justify-content-center">
+          <div class="icon compare"></div>
+          <div class="title">
+            So sánh
+          </div>
+        </div>
+        <div class="left-side_item align-items-center justify-content-center">
+          <div class="icon support"></div>
+          <div class="title">
+            Hỗ trợ khách hàng
+          </div>
+        </div>
+      </div>
+      <div class="call-now d-flex align-items-center gap-2">
+        <div class="icon"></div>
+        <div class="title">+1-202-555-0104</div>
       </div>
     </div>
   </header>
@@ -46,40 +71,22 @@
 <script>
 import InputText from 'primevue/inputtext';
 import Dropdown from 'primevue/dropdown';
-import Menu from 'primevue/menu';
-import Button from 'primevue/button';
 
 export default {
   components: {
     Dropdown,
-    InputText,
-    Menu,
-    Button,
+    InputText
   },
   data() {
     return {
-      menus: [
-        'Men',
-        'Women',
-        'Kids',
-        'Shop',
-        'Contact us'
-      ],
-      userActions: [
-        {
-          label: 'Thông tin',
-          icon: 'pi pi-refresh'
-        },
-        {
-          label: 'Đăng xuất',
-          icon: 'pi pi-upload'
-        }
+      selectedCity: null,
+      cities: [
+        {name: 'New York', code: 'NY'},
+        {name: 'Rome', code: 'RM'},
+        {name: 'London', code: 'LDN'},
+        {name: 'Istanbul', code: 'IST'},
+        {name: 'Paris', code: 'PRS'}
       ]
-    }
-  },
-  methods: {
-    toggle(event) {
-      this.$refs.menu.toggle(event);
     }
   },
 }
@@ -87,29 +94,146 @@ export default {
 
 
 <style lang="scss" scoped>
+$padding-header: 12px;
 .m-header {
   flex-shrink: 0;
-  background: #FFFFFF;
-  height: 80px;
-  padding-left: var(--padding-base);
-  padding-right: var(--padding-base);
+  background: #1B6392;
+
+  .ms-top_header {
+    padding: $padding-header var(--padding-base) $padding-header var(--padding-base);
+    color: #FFF;
+    box-shadow: 0px -1px 0px 0px rgba(255, 255, 255, 0.16) inset;
+  }
+
+  .ms-middle_header {
+    padding: $padding-header var(--padding-base) $padding-header var(--padding-base);
+  }
+
+  .ms-bottom_header {
+    background: #FFF;
+    box-shadow: 0px -1px 0px 0px #E4E7E9 inset;
+    padding: 16px var(--padding-base) 16px var(--padding-base);
+
+    .left-side {
+      .ms-category {
+        width: 154px;
+        background: #F2F4F5;
+        color: #191C1F;
+      }
+
+      .left-side_item {
+        padding: 0px 6px 0px 6px;
+        cursor: pointer;
+        display: flex;
+        height: 40px;
+        line-height: 40px;
+        align-items: center;
+        justify-content: center;
+        flex-direction: row;
+        text-wrap: nowrap;
+
+        .title {
+          padding-left: 6px;
+        }
+
+        .icon {
+          background-repeat: no-repeat;
+          background-color: transparent;
+          width: 24px;
+          height: 24px;
+        }
+
+        .map {
+          background-image: url('@public/assets/icons/MapPinLine.svg');
+        }
+
+        .compare {
+          background-image: url('@public/assets/icons/ArrowsCounterClockwise.svg');
+        }
+
+        .support {
+          background-image: url('@public/assets/icons/Headphones.svg');
+        }
+      }
+    }
+
+    .call-now {
+      .icon {
+        background-repeat: no-repeat;
+        background-color: transparent;
+        background-image: url('@public/assets/icons/PhoneCall.svg');
+        width: 28px;
+        height: 28px;
+      }
+    }
+  }
+
+  .social-contact_container {
+    .social-contact_list {
+      .social-contact_item {
+        width: 16px;
+        height: 16px;
+        background-repeat: no-repeat;
+        background-color: transparent;
+      }
+
+      .twitter {
+        background-image: url('@public/assets/icons/Twitter.svg');
+      }
+
+      .facebook {
+        background-image: url('@public/assets/icons/Facebook.svg');
+      }
+
+      .pinterest {
+        background-image: url('@public/assets/icons/Pinterest.svg');
+      }
+
+      .youtube {
+        background-image: url('@public/assets/icons/Youtube.svg');
+      }
+
+      .instagram {
+        background-image: url('@public/assets/icons/Instagram.svg');
+      }
+    }
+  }
+
+  .m-logo_container {
+    gap: 8px;
+    flex-shrink: 0;
+    padding: 1px 23.786px 1px 0px;
+    height: 48px;
+
+    .logo {
+      width: 48px;
+      height: 48px;
+      background-size: contain;
+      background-image: url('@public/assets/icons/logo_white.svg');
+      background-repeat: no-repeat;
+      background-color: transparent;
+    }
+
+    .title {
+      font-size: 32px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 40px; /* 125% */
+      letter-spacing: -0.64px;
+      color: #FFF;
+    }
+  }
 
   .m-search_form {
     max-width: 665px;
-    background: var(--Gray-50);
+    background: #fff;
     border-radius: 4px;
     height: 40px;
 
-    &:focus {
-      border-color: red;
-    }
-
-    .p-inputtext {
-      background: transparent;
-    }
-
     .ms-btn_search {
-      border-radius: 0px 4px 4px 0px;
+      border-radius: 0px;
+      border-top-right-radius: 4px;
+      border-bottom-right-radius: 4px;
     }
 
     .ms-search_form-group {
@@ -124,57 +248,56 @@ export default {
     }
   }
 
-  .m-header_right {
-    .group-profile {
-      & ~ .search-mobile {
-        display: none;
+  .m-header_actions {
+    .icon {
+      width: 32px;
+      height: 32px;
+      flex-shrink: 0;
+      background-repeat: no-repeat;
+      background-color: transparent;
+    }
+
+    .cart {
+      position: relative;
+
+      .icon {
+        background-image: url('@public/assets/icons/cart.svg');
       }
 
-      .group_user-info {
-        .ms-btn {
-          width: 120px;
-          height: 40px;
-        }
-
-        .m-user_avatar {
-          width: 40px;
-          height: 40px;
-          border-radius: 50%;
-        }
-
-        .user-info {
-          color: var(--Gray-600);
-          font-size: 16px;
-        }
+      .title {
+        position: absolute;
+        width: 16px;
+        height: 16px;
+        padding: 1px;
+        right: 0;
+        top: 0;
+        font-size: 12px;
+        text-align: center;
+        line-height: 16px;
+        color: #1B6392;
+        background: #fff;
+        border-radius: 50%;
       }
     }
+
+    .love {
+      .icon {
+        background-image: url('@public/assets/icons/Heart.svg');
+      }
+    }
+
+    .message {
+      .icon {
+        background-image: url('@public/assets/icons/message.svg');
+      }
+    }
+
+    .profile {
+      .icon {
+        background-image: url('@public/assets/icons/User.svg');
+      }
+    }
+
   }
 }
-
-@media (max-width: 768px) {
-  .m-header {
-    .m-header_left {
-      display: none !important;
-    }
-
-    .m-header_right {
-      flex: 1;
-
-      .m-header-search {
-        display: none;
-      }
-
-      .group-profile {
-        .group-icons {
-          display: none !important;
-        }
-
-        & ~ .search-mobile {
-          display: block;
-        }
-      }
-    }
-  }
-}
-
 </style>
